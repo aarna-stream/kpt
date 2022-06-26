27c1b81478c7c7932705b4df84ab356f8bd619e1
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-06-26 00:21:00 -0400

    kpt:1.0.0-beta.17

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -26,7 +26,7 @@ dl_ver() {
     local lchecksums=$DIR/$checksums
     if [ ! -e $lchecksums ];
     then
-        wget -q -O $lchecksums $url
+        curl -sSLf -o $lchecksums $url
     fi
 
     printf "  # %s\n" $url
@@ -37,4 +37,4 @@ dl_ver() {
     dl $ver $lchecksums darwin amd64
 }
 
-dl_ver ${1:-1.0.0-beta.9}
+dl_ver ${1:-1.0.0-beta.17}
